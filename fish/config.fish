bb582e7451ea5e61018dcae3f102b849ede8e013
ramintheredmn <redcrossprog@gmail.com>
2024-07-27 21:00:55 +0330

    wlogout added

--- fish/config.fish
+++ fish/config.fish
@@ -12,3 +12,17 @@ set -Ux QT_QPA_PLATFORMTHEME qt6ct
 set -Ux TERMINAL kitty
 set -Ux TERMINALEMULATOR kitty
 set PATH $PATH /home/ramin/miniconda3/bin
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/ramin/miniconda3/bin/conda
+    eval /home/ramin/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/ramin/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/home/ramin/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/ramin/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
