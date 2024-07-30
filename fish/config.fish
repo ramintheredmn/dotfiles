5ce2e499aa96bf82f5f0ba4832ed7af7749e67a3
ramintheredmn <redcrossprog@gmail.com>
2024-07-30 12:37:19 +0330

    just minor twekes

--- fish/config.fish
+++ fish/config.fish
@@ -12,7 +12,13 @@ set -Ux QT_QPA_PLATFORMTHEME qt6ct
 set -Ux TERMINAL kitty
 set -Ux TERMINALEMULATOR kitty
 set PATH $PATH /home/ramin/miniconda3/bin
+set CUDA_HOME /opt/cuda
+set PATH $PATH $CUDA_HOME/bin
+alias adgpu ~/AutoDock-GPU/bin/autodock_gpu_64wi
+set GPU_INCLUDE_PATH $CUDA_HOME/include
+set GPU_LIBRARY_PATH $CUDA_HOME/lib64
 
+set PATH $PATH ~/Desktop/personal/cheminfo/pymol/bin
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 if test -f /home/ramin/miniconda3/bin/conda
