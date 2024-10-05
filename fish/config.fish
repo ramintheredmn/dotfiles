da371b8c219fcf84adaaeba59306818aad84e6b2
ramintheredmn <redcrossprog@gmail.com>
2024-10-05 22:19:55 +0330

    new changes

--- fish/config.fish
+++ fish/config.fish
@@ -2,9 +2,9 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 # overwrite greeting
 # potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
+function fish_greeting
+    #date
+end
 
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/ramin/.ghcup/bin # ghcup-env
 
